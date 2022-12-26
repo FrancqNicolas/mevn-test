cb3c2f2063f035e60638332c785d57c07b492f60
nico <nicolas.francq98@gmail.com>
2022-12-26 18:50:00 +0100

    Fixed depreciation for mongoose and finished initial server config

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({path: '.env'});
 const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
@@ -6,7 +6,16 @@ const bodyParser = require("body-parser")
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9ju80xw.mongodb.net/?retryWrites=true&w=majority`
 const app = express()
 
-// Creating express app
+// Handling CORS policy
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE"); // If using .fetch and not axios
+    res.header("Access-Control-Allow-Headers", "auth-token, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
+
+// Express app
+mongoose.set('strictQuery', false);
 mongoose
   .connect(uri, {
     useNewUrlParser: true,
@@ -19,4 +28,14 @@ mongoose
     console.log("enculé", e);
   });
 
-  app.use(bodyParser.json())
+app.use(bodyParser.json())
+
+  // Routes
+app.get("/", (req, res) => {
+    res.send("Test")
+})
+
+
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Listening on ${process.env.APP_PORT}`)
+})
